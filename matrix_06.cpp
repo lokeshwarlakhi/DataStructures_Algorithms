4ee32b67be7cc167ddecd1382ba3dd71360799ac
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2022-01-03 20:20:55 +0530

    03-01-2022 2020

--- matrix_06.cpp
+++ matrix_06.cpp
@@ -1,9 +1,31 @@
 #include<iostream>
-#include<bit/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
+int r,c;cin>>r>>c;
+int m[r][c];
+int ct =0;
+for(int i=0;i<r;i++){
+	for(int j=0;j<c;j++){
+		cin>>m[i][j];
+		if(m[i][j]==1)
+			ct++;
+	}
+}
+if(ct%2!=0){
+	for(int i=0;i<ct;i++){
+		cout<<1<<" ";
+	}
+}
+else{
+for(int i=0;i<2;i++){
+	for(int j=0;j<ct/2;j++){
+		cout<<1<<" ";
+	}
+    cout<<endl;
+}
 
-
+}
     
 }
