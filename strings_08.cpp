088b78245423c8a5de47352d1c62f199286c0523
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2022-01-08 20:03:19 +0530

    08-01-2022 2003

--- strings_08.cpp
+++ strings_08.cpp
@@ -15,6 +15,23 @@ void c_p_c(){
 
 int main(){
     // c_p_c();
-    
+    int n;
+    cin>>n;
+    int ar[n];
+    int i=0;
+    for(i=0;i<n;i++)
+        cin>>ar[i];
+
+    for(i=0;i<n;i++){
+        if(ar[i]>ar[i+1]){
+            i-=1;
+            break;
+        }
+    }
+    int j=0;
+    for(j=i+1;j<n;j++){
+        if()
+    }
+    // cout<<i;
     return 0;
 }
